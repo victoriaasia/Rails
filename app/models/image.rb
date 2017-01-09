59b5cb14a90635834a5a4ff2bb43042a7b0a6815
V A <V A>
2017-01-09 16:34:21 +0300

    fixed vol7

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,6 @@
 class Image < ApplicationRecord
-
   belongs_to :theme
+  scope :theme_images, -> (theme_id) { select('id','name','file','ave_value').where(theme_id: theme_id) }
 
+  scope :find_image, -> (image_id) { find(image_id) }
 end
